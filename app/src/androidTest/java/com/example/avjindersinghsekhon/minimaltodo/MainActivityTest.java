1e6c789bcf58270fd35cec8585c60715c75a6f1a
VadMack <makarovvadim01@icloud.com>
2021-02-10 17:34:00 +0300

    added MainActivityTest

---
+++ app/src/androidTest/java/com/example/avjindersinghsekhon/minimaltodo/MainActivityTest.java
@@ -0,0 +1,83 @@
+package com.example.avjindersinghsekhon.minimaltodo;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.example.avjindersinghsekhon.minimaltodo.Main.MainActivity;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isClickable;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.core.AllOf.allOf;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    /*@Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }*/
+
+    @Test
+    public void checkMyCoordinatorLayout() {
+        onView(ViewMatchers.withId(R.id.myCoordinatorLayout))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkAddToDoItemFAB() {
+        onView(ViewMatchers.withId(R.id.addToDoItemFAB))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkToolbar() {
+        onView(ViewMatchers.withId(R.id.toolbar))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void isClickableAddToDoItemFAB() {
+        onView(ViewMatchers.withId(R.id.addToDoItemFAB))
+                .check(matches(isClickable()));
+    }
+
+    @Test
+    public void clickAddToDoItemFAB() {
+        onView(ViewMatchers.withId(R.id.addToDoItemFAB))
+                .perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.toDoCustomTextInput))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkActionsBar() {
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(ViewMatchers.withText(R.string.action_settings))
+                .check(matches(isDisplayed()));
+        onView(ViewMatchers.withText(R.string.about))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testOnOptionsItemSelected() {
+    }
+}
